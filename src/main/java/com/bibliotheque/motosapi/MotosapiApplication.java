802d73ed1637f521bd1119b4743dd3023a6e0ded
hamzaElhaddaoui <hamza.el.haddaoui@gmail.com>
2020-03-25 22:04:17 +0100

    initalisation de projet

---
+++ src/main/java/com/bibliotheque/motosapi/MotosapiApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.motosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotosapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotosapiApplication.class, args);
+    }
+
+}
